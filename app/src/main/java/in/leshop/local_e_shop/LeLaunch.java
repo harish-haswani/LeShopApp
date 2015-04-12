4801df4bde5b92cc4121222af17a9bce9028bf81
harish-haswani <haswani.harish@gmail.com>
2015-04-12 12:22:04 +0530

    app/le-shop: Intial commit for Local-E-Shop app.
    As of now two activities implemented:
    a. Loading Screen
    b. Connectivity not available.
    
    There are some issues in screen transition.

---
+++ app/src/main/java/in/leshop/local_e_shop/LeLaunch.java
@@ -0,0 +1,71 @@
+package in.leshop.local_e_shop;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.SupplicantState;
+import android.net.wifi.WifiInfo;
+import android.os.HandlerThread;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.net.wifi.WifiManager;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.ProgressBar;
+
+public class LeLaunch extends ActionBarActivity {
+
+    private String TAG = "LeLaunch";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_le_launch);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        HandlerThread
+        boolean isConnected = false;
+        int checkConnectivity = 0;
+        while (checkConnectivity < 10000) {
+            isConnected = isConnected();
+            if(isConnected)
+                break;
+            checkConnectivity++;
+        }
+        if (isConnected) {
+            Log.e(TAG, "Internet Connectivity is there");
+            Intent intent = new Intent(this, OutOfCoverage.class);
+            startActivity(intent);
+        }
+        else {
+            Log.e(TAG, "Internet Connectivity is not there");
+            Intent intent = new Intent(this, OutOfCoverage.class);
+            startActivity(intent);
+        }
+    }
+
+    private boolean isConnected() {
+        boolean isConnected = false;
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        isConnected = (activeNetworkInfo != null && activeNetworkInfo.isConnected());
+        if(!isConnected) {
+            SupplicantState supState;
+            WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            supState = wifiInfo.getSupplicantState();
+            if(supState.equals(SupplicantState.ASSOCIATED))
+                isConnected=true;
+        }
+        return isConnected;
+    }
+}
