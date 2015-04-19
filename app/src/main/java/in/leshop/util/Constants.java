a2aada80ca1baffb2a445dd3b3193871d6636d31
mudits2k1 <sharma.mudit@gmail.com>
2015-04-19 12:32:45 +0530

    Location And Address utilities added:

---
+++ app/src/main/java/in/leshop/util/Constants.java
@@ -0,0 +1,17 @@
+package in.leshop.util;
+
+/**
+ * Created by Mudits on 4/18/2015.
+ */
+
+public final class Constants {
+    public static final int SUCCESS_RESULT = 0;
+    public static final int FAILURE_RESULT = 1;
+    public static final String PACKAGE_NAME =
+            "in.leshop.util";
+    public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    public static final String RESULT_DATA_KEY = PACKAGE_NAME +
+            ".RESULT_DATA_KEY";
+    public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME +
+            ".LOCATION_DATA_EXTRA";
+}
